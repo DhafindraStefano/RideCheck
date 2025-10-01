56544ac126dcb0263bff8c8418d03a2f9d40352d
DhafindraStefano <Dhafindra.razaqa@gmail.com>
2025-10-01 17:35:33 +0800

    initial commit

---
+++ RideCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RideCheck
+//
+//  Created by Dhafindra Razaqa Stefano on 01/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RideCheck/RideCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideCheckApp.swift
+//  RideCheck
+//
+//  Created by Dhafindra Razaqa Stefano on 01/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RideCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
