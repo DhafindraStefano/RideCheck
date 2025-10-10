586eb564317cb8975d1e26a799ce71601fb04b57
DhafindraStefano <Dhafindra.razaqa@gmail.com>
2025-10-10 11:51:57 +0800

    onboarding pages setup

---
+++ RideCheck/View/Onboarding/SplashScreen/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  RideCheck
+//
+//  Created by Dhafindra Razaqa Stefano on 10/10/25.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
---
+++ RideCheck/View/Onboarding/VehicleSetup/VehicleSetupView.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleSetupView.swift
+//  RideCheck
+//
+//  Created by Dhafindra Razaqa Stefano on 10/10/25.
+//
+
+import SwiftUI
+
+struct VehicleSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VehicleSetupView()
+}
